51fc003f5934ed07e4fbc74a182ecb8bb2a19271
Adam Tiller <a.j.tiller@gmail.com>
2020-12-03 08:55:49 -0500

    Day 3 cleanup.

--- day3.js
+++ day3.js
@@ -2,41 +2,25 @@ const fs = require('fs');
 
 const input = fs.readFileSync(__dirname + '/inputs/day3.input', 'utf8').split('\n');
 
-const treeMatrix = [];
+function tobogganCrashes(rightOffset, downOffset, terrain) {
+  const altitude = terrain.length;
+  const repeatWidth = terrain[0].length;
 
-for (const treeString of input) {
-  treeMatrix.push(treeString.split(''));
-}
+  let row = 0;
+  let col = 0;
+  let collisions = 0;
 
-let row = 0;
-let col = 0;
-let trees = 0;
-
-while (row < treeMatrix.length) {
-  if (treeMatrix[row][col] === '#') trees++;
-  row++;
-  col = (col + 3) % treeMatrix[0].length;
-}
-
-console.log(trees)
-
-const offsets = [[1,1], [3,1], [5,1], [7,1], [1,2]];
-const results = []
-
-for (const offset of offsets) {
-  row = 0;
-  col = 0;
-  trees = 0;
-
-  while (row < treeMatrix.length) {
-    if (treeMatrix[row][col] === '#') trees++;
-    row += offset[1];
-    col = (col + offset[0]) % treeMatrix[0].length;
+  while (row < altitude) {
+    if (terrain[row][col] === '#') collisions++;
+    row = row + downOffset;
+    col = (col + rightOffset) % repeatWidth;
   }
 
-  results.push(trees);
+  return collisions;
 }
 
-console.log(results.reduce((a, v) => (a * v)))
+console.log('Part 1: ', tobogganCrashes(3, 1, input));
 
+const slopes = [[1,1], [3,1], [5,1], [7,1], [1,2]];
 
+console.log('Part 2: ', slopes.reduce((accumulator, [right, down]) => (accumulator * tobogganCrashes(right, down, input)), 1));
