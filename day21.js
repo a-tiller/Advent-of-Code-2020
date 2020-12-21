8323f1a8006005506060e19cb650c96e2e003020
Adam Tiller <a.j.tiller@gmail.com>
2020-12-21 16:58:37 -0500

    Day 21 Solution.

---
+++ day21.js
@@ -0,0 +1,98 @@
+const fs = require('fs');
+
+const input = fs.readFileSync(__dirname + '/inputs/day21.input', 'utf8').split('\n').map(food => food.split(' (contains '));
+
+function parseInput(foodList) {
+  const ingredientToAllergens = new Map();
+  const allergenToIngredients = new Map();
+  const ingredientCounts = new Map();
+
+  for (const [ingredientList, allergenList] of foodList) {
+    const ingredients = ingredientList.split(' ');
+    const allergens = allergenList.slice(0, -1).split(', ');
+
+    for (const ingredient of ingredients) {
+      if (!ingredientCounts.has(ingredient)) {
+        ingredientCounts.set(ingredient, 1);
+      } else {
+        ingredientCounts.set(ingredient, ingredientCounts.get(ingredient) + 1);
+      }
+
+      if (!ingredientToAllergens.has(ingredient)) {
+        ingredientToAllergens.set(ingredient, new Set());
+      }
+      for (const allergen of allergens) {
+        ingredientToAllergens.get(ingredient).add(allergen);
+      }
+    }
+
+    for (const allergen of allergens) {
+      if (!allergenToIngredients.has(allergen)) {
+        allergenToIngredients.set(allergen, new Set());
+        for (const ingredient of ingredients) {
+          allergenToIngredients.get(allergen).add(ingredient);
+        }
+      } else {
+        for (const ingredient of allergenToIngredients.get(allergen)) {
+          if (!ingredients.includes(ingredient)) {
+            allergenToIngredients.get(allergen).delete(ingredient)
+          }
+        }
+      }
+    }
+  }
+
+  return { ingredientCounts, ingredientToAllergens, allergenToIngredients };
+}
+
+function deduceAllegens(allergenToIngredients) {
+  console.log(allergenToIngredients.keys())
+  const workingCopy = new Map(allergenToIngredients);
+
+  const allergenToIngredient = {};
+
+  while (workingCopy.size) {
+    workingCopy.forEach((ingredients, allergen) => {
+      if (ingredients.size === 1) {
+        const ingredient = [...ingredients][0];
+
+        allergenToIngredient[allergen] = ingredient;
+        workingCopy.forEach((innerIngredients, _) => {
+          innerIngredients.delete(ingredient);
+        });
+      }
+
+      if (ingredients.size === 0) workingCopy.delete(allergen);
+    });
+  }
+
+  return allergenToIngredient;
+}
+
+function makeCanonicalList(allergenToIngredient) {
+  console.log(allergenToIngredient)
+
+  return Object.entries(allergenToIngredient)
+            .sort(([allergenA, ingredientA], [allergenB, ingredientB]) => (allergenA > allergenB ? 1 : -1))
+            .map(([allergen, ingredient]) => (ingredient))
+            .join(',');
+}
+
+const { ingredientCounts, ingredientToAllergens, allergenToIngredients } = parseInput(input);
+
+let sum = [...ingredientCounts.values()].reduce((a, b) => (a + b));
+
+const possibleAllergenIngredients = new Set();
+
+for (const ingredientList of allergenToIngredients.values()) {
+  for (const ingredient of ingredientList) {
+    possibleAllergenIngredients.add(ingredient);
+  }
+}
+
+for (const ingredient of [...possibleAllergenIngredients]) {
+  sum -= ingredientCounts.get(ingredient);
+}
+
+console.log("Part 1: ", sum);
+console.log("Part 2: ", makeCanonicalList(deduceAllegens(allergenToIngredients)));
